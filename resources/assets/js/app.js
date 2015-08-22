18f4837598b9db1222630ccd2bb312ff74287c7e
Zeuxis Lo <seekstudio@gmail.com>
2015-08-23 03:08:35 +0800

    add vue and cue router for prototype layout

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,3 +1,38 @@
-(function($) {
+var Vue = require('vue'),
+    VueRouter = require('vue-router');
 
-})(jQuery);
+//
+var DummyComponent = Vue.extend({
+    template: '<p>This is Test! {{ $route.params.category }}</p>'
+});
+
+//
+Vue.use(VueRouter);
+
+var Router = new VueRouter({
+    history: true,
+    saveScrollPosition: true
+});
+
+Router.map({
+    '/': {
+        component: DummyComponent
+    },
+
+    '/inform': {
+        component: DummyComponent
+    },
+
+    '/c/:category': {
+        component: DummyComponent
+    }
+})
+
+var App = Vue.extend({
+    template: require('./template/index.html'),
+    ready   : function() {
+        $(".button-collapse").sideNav();
+    }
+});
+
+Router.start(App, '#app');
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,13 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <title>Rowky</title>
+<link href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en" rel="stylesheet">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link rel="stylesheet" href="{{ elixir('assets/css/bundle.css') }}">
 </head>
 <body>
 <div id="app"></div>
 <script src="{{ elixir('assets/js/bundle.js') }}"></script>
+<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
 </body>
 </html>
