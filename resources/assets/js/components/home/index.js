065074fe8bef0590c3ebdc82a9c760d85719a555
Zeuxis Lo <seekstudio@gmail.com>
2015-08-24 19:21:01 +0800

    add inform page layout

--- resources/assets/js/components/home/index.js
+++ resources/assets/js/components/home/index.js
@@ -1,5 +1,8 @@
 require('./style.scss');
 
 module.exports = {
-    template: require('./template.html')
+    template: require('./template.html'),
+    ready   : function() {
+        $('select').material_select();
+    }
 }
