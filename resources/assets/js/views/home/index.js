2856e24cb16d6e732bec087ec5e3426637391705
Zeuxis Lo <seekstudio@gmail.com>
2015-09-11 19:23:32 +0800

    make front-end pagination work with backend pagination meta

--- resources/assets/js/views/home/index.js
+++ resources/assets/js/views/home/index.js
@@ -3,7 +3,8 @@ module.exports = {
 
     data: function() {
         return {
-            informs: []
+            informs    : [],
+            pagination : {}
         }
     },
 
@@ -15,12 +16,46 @@ module.exports = {
                 pagination = response.meta.pagination;
 
             this.informs = informs;
+            this.pagination = pagination;
         });
+    },
+
+    computed: {
+        hasPreviousPage: function() {
+            return this.pagination.current_page - 1 > 0;
+        },
+
+        hasNextPage: function() {
+            return this.pagination.current_page + 1 <= this.pagination.total_pages;
+        },
     },
 
     methods: {
         onChangeCategory: function(value) {
             console.log(value);
+        },
+
+        switchPage: function(action) {
+            var page_no = 0;
+
+            switch(action) {
+                case 'previous':
+                    page_no = this.pagination.current_page - 1;
+                    break;
+                case 'next':
+                    page_no = this.pagination.current_page + 1;
+                    break;
+            }
+
+            this.$api.inform.all({
+                page: page_no
+            }, function(response) {
+                var informs    = response.data,
+                pagination = response.meta.pagination;
+
+                this.informs = informs;
+                this.pagination = pagination;
+            });
         }
     },
 
