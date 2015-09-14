4fc7879fb48d7651c1ca969a0ae3a8ee06fd9951
Zeuxis Lo <seekstudio@gmail.com>
2015-09-14 14:03:07 +0800

    remove top navbar categories

--- resources/assets/js/views/layout/index.js
+++ resources/assets/js/views/layout/index.js
@@ -1,18 +1,6 @@
 module.exports = {
     template: require('./template.html'),
 
-    data: function() {
-        return {
-            categories: []
-        }
-    },
-
-    compiled: function() {
-        this.$api.category.all({}, function(response) {
-            this.categories = response.data;
-        });
-    },
-
     ready: function() {
         $(document).foundation();
     }
